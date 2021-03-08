30b5fc23a17542b82bda413ad22c28b5e8d4f7e6
konstantinos sfikas <kon.sfik@gmail.com>
2021-03-08 17:20:48 +0200

    Added a template for a custom mutation method

---
+++ Level_Generation_Tutorial_Java_Project/src/genetic_algorithm/mutation_methods/MM__Template.java
@@ -0,0 +1,23 @@
+package genetic_algorithm.mutation_methods;
+
+import java.util.Random;
+
+import genetic_algorithm.Level_Individual;
+
+public class MM__Template extends Mutation_Method
+{
+
+	@Override
+	public void Mutate_Individual(Random rand, Level_Individual individual)
+	{
+		/**
+		 * TODO: Use this class to define your own mutation method. This method should
+		 * operate directly on the individual and (slightly) alter its structure. For
+		 * example, you may change the values of a percentage of cells, by flipping
+		 * them. Alternatively, you may select random regions and set them to walls or
+		 * floors, at random. Make sure to take a look at the already implemented
+		 * method(s) as an example.
+		 */
+	}
+
+}
